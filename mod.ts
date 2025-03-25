840e973fba9385b49fb080d32d6cddfc8499b476
Jacek Fiszer <jackfiszr+gh@gmail.com>
2025-03-25 19:23:31 +0100

    docs(mod): remove @module jsdoc

--- mod.ts
+++ mod.ts
@@ -1,7 +1,3 @@
-/**
- * This module provides a collection of utility functions for various purposes.
- * @module
- */
 export * from "./logger.ts";
 export * from "./misc.ts";
 export * from "./start_app.ts";
